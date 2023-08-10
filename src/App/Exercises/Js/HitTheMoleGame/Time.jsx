2893f8d01ad71be52edcadb1320e0322ef39e17a
novelus1 <krzysztof.truszkowski@outlook.com>
2023-08-10 17:42:13 +0200

    Delete mole for PR

--- src/App/Exercises/Js/HitTheMoleGame/Time.jsx
+++
@@ -1,9 +0,0 @@
-const formatTime = (totalSeconds) => {
-  const date = new Date(totalSeconds * 1000);
-
-  const formattedTime = date.toISOString().substring(14, 19);
-
-  return formattedTime;
-};
-
-export default formatTime;
